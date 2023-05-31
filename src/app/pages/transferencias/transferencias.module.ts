0da1277986cd0b24d524640ec9a1552de46c790d
Gabriel Franco <francogabrielpy@outlook.com>
2023-05-31 14:05:00 -0400

    from v14 to v15

--- src/app/pages/transferencias/transferencias.module.ts
+++ src/app/pages/transferencias/transferencias.module.ts
@@ -1,16 +1,15 @@
-import { ModificarItemDialogComponent } from './modificar-item-dialog/modificar-item-dialog.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { IngresarCodigoPopComponent } from './ingresar-codigo-pop/ingresar-codigo-pop.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { ComponentsModule } from './../../components/components.module';
 import { InfoTransferenciaComponent } from './info-transferencia/info-transferencia.component';
-import { TransferenciaComponent } from './transferencia/transferencia.component';
+import { IngresarCodigoPopComponent } from './ingresar-codigo-pop/ingresar-codigo-pop.component';
 import { ListTransferenciasComponent } from './list-transferencias/list-transferencias.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ModificarItemDialogComponent } from './modificar-item-dialog/modificar-item-dialog.component';
+import { TransferenciaComponent } from './transferencia/transferencia.component';
 
-import { TransferenciasRoutingModule } from './transferencias-routing.module';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { TransferenciasRoutingModule } from './transferencias-routing.module';
 
 
 @NgModule({
@@ -21,7 +20,7 @@ import { IonicModule } from '@ionic/angular';
     TransferenciasRoutingModule,
     ComponentsModule,
     ReactiveFormsModule,
-    FormsModule,
+    FormsModule
   ],
   exports: [ListTransferenciasComponent, TransferenciaComponent],
 
