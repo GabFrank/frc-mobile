0912c5ca55d43324974e532ebda3e9dcf8b0dce5
Gabriel Franco <francogabrielpy@outlook.com>
2024-09-30 17:28:11 -0400

    cambio en modificar item

--- src/app/pages/transferencias/modificar-item-dialog/modificar-item-dialog.component.ts
+++ src/app/pages/transferencias/modificar-item-dialog/modificar-item-dialog.component.ts
@@ -1,7 +1,11 @@
 import { PopOverService } from './../../../services/pop-over.service';
 import { Form, FormControl, Validators } from '@angular/forms';
 import { Component, Input, OnInit } from '@angular/core';
-import { EtapaTransferencia, Transferencia, TransferenciaItem } from '../transferencia.model';
+import {
+  EtapaTransferencia,
+  Transferencia,
+  TransferenciaItem
+} from '../transferencia.model';
 import { Presentacion } from 'src/app/domains/productos/presentacion.model';
 import { PresentacionService } from 'src/app/services/presentacion/presentacion.service';
 
@@ -14,10 +18,9 @@ export interface ModificarItemData {
 @Component({
   selector: 'app-modificar-item-dialog',
   templateUrl: './modificar-item-dialog.component.html',
-  styleUrls: ['./modificar-item-dialog.component.scss'],
+  styleUrls: ['./modificar-item-dialog.component.scss']
 })
 export class ModificarItemDialogComponent implements OnInit {
-
   cantidadAnterior;
   vencimientoAnterior;
   cantidadControl;
@@ -31,56 +34,87 @@ export class ModificarItemDialogComponent implements OnInit {
   isCantidad = false;
   isVencimiento = false;
 
-  constructor(private popoverService: PopOverService, private presentacionService: PresentacionService) { }
+  constructor(
+    private popoverService: PopOverService,
+    private presentacionService: PresentacionService
+  ) {
+    this.cantidadControl = new FormControl(null);
+    this.vencimientoControl = new FormControl(null, [Validators.required]);
+    this.presentacionControl = new FormControl(null);
+  }
 
   async ngOnInit() {
-    if(this.data?.item?.presentacionPreTransferencia != null){
-     (await this.presentacionService.onGetPresentacionesPorProductoId(this.data.item.presentacionPreTransferencia?.producto?.id)).subscribe(presentacionRes => {
-      if(presentacionRes?.length > 0){
-        this.presentacionList = presentacionRes;
-        // this.presentacionControl = new FormControl(this.presentacionList.find(p => p.id == this.data.item.presentacionPreTransferencia?.id), [Validators.required])
-      }
-     })
+    if (this.data?.item?.presentacionPreTransferencia != null) {
+      (
+        await this.presentacionService.onGetPresentacionesPorProductoId(
+          this.data.item.presentacionPreTransferencia?.producto?.id
+        )
+      ).subscribe((presentacionRes) => {
+        if (presentacionRes?.length > 0) {
+          this.presentacionList = presentacionRes;
+          // this.presentacionControl = new FormControl(this.presentacionList.find(p => p.id == this.data.item.presentacionPreTransferencia?.id), [Validators.required])
+        }
+      });
     }
-    this.cantidadControl = new FormControl(null)
-    this.vencimientoControl = new FormControl(null, [Validators.required])
-    this.presentacionControl = new FormControl(null);
 
-    if(this.data.isCantidad){
+    if (this.data.isCantidad) {
       this.isCantidad = true;
-    } else if(this.data.isVencimiento){
+    } else if (this.data.isVencimiento) {
       this.isVencimiento = true;
     }
-    switch(this.data.transferencia.etapa){
+    switch (this.data.transferencia.etapa) {
       case EtapaTransferencia.PREPARACION_MERCADERIA:
-        this.cantidadAnterior =   this.data.item?.cantidadPreparacion || this.data.item.cantidadPreTransferencia;
-        this.vencimientoAnterior = this.data.item?.vencimientoPreparacion || this.data.item.vencimientoPreTransferencia;
-        this.presentacionAnterior = this.data.item?.presentacionPreparacion || this.data.item.presentacionPreTransferencia;
+        this.cantidadAnterior =
+          this.data.item?.cantidadPreparacion ||
+          this.data.item.cantidadPreTransferencia;
+        this.vencimientoAnterior =
+          this.data.item?.vencimientoPreparacion ||
+          this.data.item.vencimientoPreTransferencia;
+        this.presentacionAnterior =
+          this.data.item?.presentacionPreparacion ||
+          this.data.item.presentacionPreTransferencia;
         break;
       case EtapaTransferencia.TRANSPORTE_VERIFICACION:
-        this.cantidadAnterior = this.data.item?.cantidadTransporte | this.data.item.cantidadPreparacion;
-        this.vencimientoAnterior = this.data.item?.vencimientoTransporte || this.data.item.vencimientoPreparacion;
-        this.presentacionAnterior = this.data.item?.presentacionTransporte || this.data.item.presentacionPreparacion;
+        this.cantidadAnterior =
+          this.data.item?.cantidadTransporte ||
+          this.data.item.cantidadPreparacion;
+        this.vencimientoAnterior =
+          this.data.item?.vencimientoTransporte ||
+          this.data.item.vencimientoPreparacion;
+        this.presentacionAnterior =
+          this.data.item?.presentacionTransporte ||
+          this.data.item.presentacionPreparacion;
         break;
-        case EtapaTransferencia.RECEPCION_EN_VERIFICACION:
-        this.cantidadAnterior = this.data.item?.cantidadRecepcion | this.data.item.cantidadTransporte;
-        this.vencimientoAnterior = this.data.item?.vencimientoRecepcion || this.data.item.vencimientoTransporte;
-        this.presentacionAnterior = this.data.item?.presentacionRecepcion || this.data.item.presentacionTransporte;
+      case EtapaTransferencia.RECEPCION_EN_VERIFICACION:
+        this.cantidadAnterior =
+          this.data.item?.cantidadRecepcion || this.data.item.cantidadTransporte;
+        this.vencimientoAnterior =
+          this.data.item?.vencimientoRecepcion ||
+          this.data.item.vencimientoTransporte;
+        this.presentacionAnterior =
+          this.data.item?.presentacionRecepcion ||
+          this.data.item.presentacionTransporte;
         break;
       default:
         break;
     }
   }
 
-  onAceptar(){
-    if(this.cantidadControl.value == null) this.cantidadControl.setValue(this.cantidadAnterior);
-    if(this.vencimientoControl.value == null) this.vencimientoControl.setValue(this.vencimientoAnterior);
-    if(this.presentacionControl.value == null) this.presentacionControl.setValue(this.presentacionAnterior);
-    this.popoverService.close({cantidad: this.cantidadControl.value, vencimiento: this.vencimientoControl.value, presentacion: this.presentacionControl.value})
+  onAceptar() {
+    if (this.cantidadControl.value == null)
+      this.cantidadControl.setValue(this.cantidadAnterior);
+    if (this.vencimientoControl.value == null)
+      this.vencimientoControl.setValue(this.vencimientoAnterior);
+    if (this.presentacionControl.value == null)
+      this.presentacionControl.setValue(this.presentacionAnterior);
+    this.popoverService.close({
+      cantidad: this.cantidadControl.value,
+      vencimiento: this.vencimientoControl.value,
+      presentacion: this.presentacionControl.value
+    });
   }
 
-  onCancel(){
-    this.popoverService.close(null)
+  onCancel() {
+    this.popoverService.close(null);
   }
-
 }
